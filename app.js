839125bbd8b9d1ee1c812263f1a888e45383a0be
unknown <hajdumarcel@gmail.com>
2021-02-16 11:26:21 +0100

    added basic calculator functions and an operator function

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("hello");
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, operator) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return subtract(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  }
+}
